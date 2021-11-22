56d54037de8a1eb1430de06f840b58e0c75aadc2
AndriiMysko <usandrewmisko@gmail.com>
2021-11-22 17:26:22 +0200

    Scan log modal WIP

--- app/components/insights/scan-log-modal.js
+++ app/components/insights/scan-log-modal.js
@@ -1,22 +1,31 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { observer } from '@ember/object';
+import { computed, observer } from '@ember/object';
 
 export default Component.extend({
   api: service(),
 
   scanLogs: [],
+  scanLogsPresentable: computed('scanLogs.[]', function () {
+    return this.scanLogs.map((el) => (
+      {
+        key: Object.keys(el)[0],
+        value: Object.values(el)[0]
+      }
+    ));
+  }),
   scanInProgress: false,
+  collapseValues: [],
 
   modalOpenObserver: observer('isOpen', function () {
     if (this.isOpen) {
       this.plugin.getScanLogs.perform().then((data) => {
-        let node = this.scanLogs.pop();
+        let node = this.scanLogs.popObject();
         data.scan_logs.forEach((scanLog) => {
           let logType = scanLog.log_type;
           if (!node || !node[logType]) {
             if (node) {
-              this.scanLogs.push(node);
+              this.scanLogs.pushObject(node);
             }
             node = {};
             node[logType] = [];
@@ -36,12 +45,28 @@ export default Component.extend({
             let index = node[logType].findIndex(scanLogT => scanLogT.test_template_id == scanLog.test_template_id);
             node[logType].splice(++index, 0, item);
           } else {
-            node[logType].push(item);
+            node[logType].pushObject(item);
           }
         });
-        this.scanLogs.push(node);
+        this.scanLogs.pushObject(node);
         this.set('scanInProgress', data.meta.scan_status_in_progress);
       });
     }
   }),
+
+  actions: {
+    collapseSection(evt) {
+      if (evt) {
+        let btn = document.getElementById(evt.target.id);
+        let section = evt.target.parentElement.nextElementSibling;
+        if (btn.classList.contains('collapsed')) {
+          btn.classList.remove('collapsed');
+          section.classList.remove('collapsedSection');
+        } else {
+          btn.classList.add('collapsed');
+          section.classList.add('collapsedSection');
+        }
+      }
+    }
+  }
 });
